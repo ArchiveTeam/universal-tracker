b0c20d06565b7b029fd831e98c6ae7b765bb3bcb
Alard <alard@example.com>
2011-11-11 13:23:39 +0100

    Use heroku app name.

--- set_vars_heroku.rb
+++ set_vars_heroku.rb
@@ -1,6 +1,12 @@
 require "rubygems"
 require "json"
 
+heroku_app = ARGV[0]
+if heroku_app.nil?
+  puts "Usage: set_vars_heroku.rb <NAME OF HEROKU APP>"
+  exit
+end
+
 redis_conf = JSON.parse(File.read("redis.json"))
 tracker_conf = JSON.parse(File.read("tracker.json"))
 
@@ -10,6 +16,6 @@ redis_conf.each do |key, value|
 end
 env_vars["tracker_config"] = JSON.dump(tracker_conf)
 
-system("heroku", "config:add",
+system("heroku", "config:add", "--app", heroku_app,
        *env_vars.map{ |key,value| "#{key}=#{value}" })
 
