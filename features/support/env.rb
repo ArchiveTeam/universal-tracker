4836b97a2c39e1cf97ec7c2b8d6d45e966ed26ff
David Yip <yipdw@member.fsf.org>
2011-11-21 01:35:25 -0600

    Cucumber test harness.

---
+++ features/support/env.rb
@@ -0,0 +1,34 @@
+require File.expand_path('../../../app', __FILE__)
+
+require 'rack/test'
+require 'redis'
+
+REDIS_CONFIG_PATH = File.expand_path('../redis_conf.rb', __FILE__)
+
+World do
+  Class.new do
+    include Rack::Test::Methods
+
+    def app
+      App
+    end
+  end.new
+end
+
+AfterConfiguration do
+  conf = if File.exists?(REDIS_CONFIG_PATH)
+           eval(File.read(REDIS_CONFIG_PATH))
+         else
+           { :host => 'localhost', :port => 6379, :db => 1 }
+         end
+
+  $redis = Redis.new(conf)
+end
+
+Before do
+  $redis.flushall
+end
+
+at_exit do
+  $redis.quit
+end
