e4cbc2ac4aa4a8c81fd2d8e41cc62c5d1a421f52
Alard <alard@example.com>
2011-12-02 11:28:43 +0100

    Move utility scripts to scripts/ subdirectory.

---
+++ scripts/set_vars_dotcloud.rb
@@ -0,0 +1,22 @@
+require "rubygems"
+require "json"
+
+dotcloud_app = ARGV[0]
+if dotcloud_app.nil?
+  puts "Usage: set_vars_dotcloud.rb NAME-OF-DOTCLOUD-APP [ENVIRONMENT]"
+  exit
+end
+
+environment = ARGV[1] || ENV["RACK_ENV"] || "production"
+
+REDIS_CONFIG_PATH = File.expand_path("../config/redis.json", __FILE__)
+redis_conf = JSON.parse(File.read(REDIS_CONFIG_PATH))
+
+env_vars = {}
+redis_conf[environment].each do |key, value|
+  env_vars["redis_#{ key }"] = value
+end
+
+system("dotcloud", "var", "set", dotcloud_app,
+       *env_vars.map{ |key,value| "#{key}=#{value}" })
+
---
+++ scripts/set_vars_heroku.rb
@@ -0,0 +1,22 @@
+require "rubygems"
+require "json"
+
+heroku_app = ARGV[0]
+if heroku_app.nil?
+  puts "Usage: set_vars_heroku.rb NAME-OF-HEROKU-APP [ENVIRONMENT]"
+  exit
+end
+
+environment = ARGV[1] || ENV["RACK_ENV"] || "production"
+
+REDIS_CONFIG_PATH = File.expand_path("../config/redis.json", __FILE__)
+redis_conf = JSON.parse(File.read(REDIS_CONFIG_PATH))
+
+env_vars = {}
+redis_conf[environment].each do |key, value|
+  env_vars["redis_#{ key }"] = value
+end
+
+system("heroku", "config:add", "--app", heroku_app,
+       *env_vars.map{ |key,value| "#{key}=#{value}" })
+
