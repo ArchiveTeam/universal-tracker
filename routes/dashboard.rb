0245da38119ee5da9a9d42aea0b29a0163f800ae
Alard <alard@example.com>
2013-03-14 17:42:46 -0400

    Gzip the large dashboard JSON responses.

--- routes/dashboard.rb
+++ routes/dashboard.rb
@@ -1,3 +1,6 @@
+require "stringio"
+require "zlib"
+
 module UniversalTracker
   class App < Sinatra::Base
     get "/:slug" do |slug|
@@ -12,29 +15,21 @@ module UniversalTracker
     get "/:slug/stats.json" do
       content_type :json
       expires 60, :public, :must_revalidate
+      headers["Content-Encoding"] = "gzip"
 
-      cache_key = "cache:#{ tracker.slug }:stats.json"
-      cached = redis.get(cache_key)
-      if cached.nil?
-        cached = JSON.dump(tracker.stats)
-        redis.set(cache_key, cached)
-        redis.expire(cache_key, 60)
+      gzip_cached("cache:#{ tracker.slug }:stats.json.gz") do
+        JSON.dump(tracker.stats)
       end
-      cached
     end
 
     get "/:slug/update-status.json" do
       content_type :json
       expires 60, :public, :must_revalidate
+      headers["Content-Encoding"] = "gzip"
 
-      cache_key = "cache:#{ tracker.slug }:update-status.json"
-      cached = redis.get(cache_key)
-      if cached.nil?
-        cached = JSON.dump(tracker.downloader_update_status)
-        redis.set(cache_key, cached)
-        redis.expire(cache_key, 60)
+      gzip_cached("cache:#{ tracker.slug }:update-status.json.gz") do
+        JSON.dump(tracker.downloader_update_status)
       end
-      cached
     end
 
     get "/:slug/rescue-me" do
@@ -49,6 +44,25 @@ module UniversalTracker
       tracker.log_added_items(items, request.ip)
 
       erb :rescue_me_thanks, :locals=>{ :new_items=>new_items }
+    end
+
+    private
+
+    def gzip_cached(cache_key)
+      cached = redis.get(cache_key)
+      if cached.nil?
+        cached = StringIO.new.tap do |io|
+          gz = Zlib::GzipWriter.new(io)
+          begin
+            gz.write(yield)
+          ensure
+            gz.close
+          end
+        end.string
+        redis.set(cache_key, cached)
+        redis.expire(cache_key, 60)
+      end
+      cached
     end
   end
 end
