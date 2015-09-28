0fed46b8a6cf6bb2647f72d1138a052c32d77d2b
Christopher Foo <chris.foo@gmail.com>
2015-09-27 22:42:33 -0400

    redis: Raise ConfigError. Add reading Warrior HQ config.

--- models/redis_connection.rb
+++ models/redis_connection.rb
@@ -4,15 +4,19 @@ require "redis/connection/hiredis"
 require "redis"
 
 REDIS_CONFIG_PATH = File.expand_path("../../config/redis.json", __FILE__)
+WARRIOR_REDIS_CONFIG_PATH = File.expand_path("../../config/warrior_redis.json", __FILE__)
 
 module UniversalTracker
+  class ConfigError < StandardError
+  end
+
   class RedisConnection
     class << self
       def connection
         @redis ||= Redis.new(config)
       end
 
-      def load_config(environment=ENV["RACK_ENV"])
+      def load_config(environment=ENV["RACK_ENV"], config_path=REDIS_CONFIG_PATH)
         @config = if ENV["redis_host"]
           {
             :host=>ENV["redis_host"],
@@ -21,21 +25,37 @@ module UniversalTracker
             :db=>ENV["redis_db"]
           }
 
-        elsif File.exists?(REDIS_CONFIG_PATH)
-          configs = JSON.parse(File.read(REDIS_CONFIG_PATH))
+        elsif File.exists?(config_path)
+          configs = JSON.parse(File.read(config_path))
           configs.symbolize_keys!
           if not configs.has_key?(environment.to_sym)
-            raise "No Redis configuration for the #{environment} environment."
+            raise ConfigError.new("No Redis configuration for the #{environment} environment.")
           end
           configs[environment.to_sym].symbolize_keys!
 
         else
-          raise "Missing Redis configuration."
+          raise ConfigError.new("Missing Redis configuration.")
         end
       end
 
       def config
         @config || load_config
+      end
+    end
+  end
+
+  class WarriorHQRedisConnection < RedisConnection
+    class << self
+      def connection
+        begin
+          @redis ||= Redis.new(config)
+        rescue ConfigError
+          puts "Warrior HQ not configured"
+        end
+      end
+
+      def load_config(environment=ENV["RACK_ENV"], config_path=WARRIOR_REDIS_CONFIG_PATH)
+        super(environment, config_path)
       end
     end
   end
